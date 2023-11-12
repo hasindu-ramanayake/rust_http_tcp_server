e2267c59eb851903344eed83a8ca9d1c27f34c98
rama <hawarama344@gmail.com>
2023-11-12 16:02:26 +0530

    simple server http tcp

---
+++ server/src/main.rs
@@ -0,0 +1,44 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use std::fs;
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buff: [u8; 1024] = [0; 1024];
+    stream.read(&mut buff).unwrap();
+    
+    let get: &[u8; 16] = b"GET / HTTP/1.1\r\n";
+    
+    let (status,file_name) = if buff.starts_with(get) {
+        ( "HTTP/1.1 200 OK", "index.html")
+    } else {
+        ( "HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+
+    let content = fs::read_to_string(file_name).unwrap();
+    let res = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status,
+        content.len(),
+        content
+    ); 
+
+    stream.write(res.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    
+
+}
+
+
+
+fn main() {
+    
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    for stream in listener.incoming() {
+        let strm = stream.unwrap();
+        println!("Connection established");
+        handle_connection(strm);
+    }
+}
